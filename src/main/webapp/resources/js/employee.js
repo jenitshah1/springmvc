ffebdcd3ebb4fcf2223a01ce9cbe68d0c68e3a81
jenitshah1 <jenit@gmail.com>
2018-08-02 11:27:12 +0530

    employee detail by jquery

--- src/main/webapp/resources/js/employee.js
+++ src/main/webapp/resources/js/employee.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "http://localhost:8080/springmvc/rest/emp/1"
     }).then(function(data) {
-       $('.emp-id').append(data.id);
-       $('.emp-name').append(data.name);
+       $('#emp-id').append("The Employee ID :"+data.id);
+       $('#emp-name').append("The Employee Name :"data.name);
     });
 });
